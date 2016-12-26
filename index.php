783468f5e53fbdc6d2f463406b83b70dfe1d69ec
CheSyu\Syu <choijh00623@gmail.com>
2016-12-26 15:35:26 +0900

    바디의 리스트를 모두 지웠음

--- index.php
+++ index.php
@@ -7,26 +7,6 @@
         <header>
             <h1>Git</h1>
         </header>
-        <ul>
-          commit
-            <li>sync.egoing</li>
-            <li>introduce Git</li>
-            <li>version</li>
-            <li>Branch</li>
-            <li>브랜치</li>
-            <li>충돌</li>
-            <li>충돌해결</li>
-            <li>원격저장소</li>
-            <li>push</li>
-            <li>pull</li>
-            <li>협업</li>
-            <li>sync</li>
-            <li>push.egoing</li>
-            <li>pull.leezche</li>
-            <li>conflict.egoing.leezche</li>
-            <li>외부도구.master</li>
-            <li>스테시</li>
-            <li>ignore</li>
-        </ul>
+  리스트를 모두 지움
     </body>
 </html>
